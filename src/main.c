746a32852a260e769c2830745d0efb33b965ec1f
Zellane <muguang233@gmail.com>
2025-04-29 19:25:21 +1000

    添加?指令输出

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "../include/file.h"
 //////////////////////////////////////
               //宏定义
 /////////////////////////////////////
@@ -10,23 +12,23 @@
 #define MAX_CMD 256
 #define FAIL_MEM "分配内存失败\n"
 #define MAX_ARG 5
-
+#define command_count 4
 //////////////////////////////////////
               //结构体
 //////////////////////////////////////
 struct command {
   char *cmd;
   int max_arg;
-  char *args_example;
+  char *arg_example;
   char *help_msg;
+  bool strict_args;
 };
 
 static struct command Command_list[] = {
-  {"q", 0, "", "退出程序"},
-  {"?", 1, "? {type}", "获取帮助"},
-  {"l", 2, "l {arg1} {arg2}", ""},
-  {"m", 0, "", "你怎么知道Mewscat是抖m"},
-  {"t", 3, "", "测试测试"}
+  {"q",     0, "{N/A}",               "退出程序", },
+  {"?",     2, "{类型} {进阶}",       "获取帮助(数据结构指令为类型)"},
+  {"list",  2, "{行动} {值}",         "链表操作"},
+  {"t",     3, "{N/A} {N/A} {N/A}",   "测试测试"}
 };
 
 
@@ -47,6 +49,12 @@ int get_command_index();
 
 //测试参数
 void test_arg(int argc, char *arg[]);
+
+//执行指令
+void run_command(int argc, char *arg[], char *cmd);
+
+//帮助指令
+void help(int argc, char *arg[]);
 
 
 //////////////////////////////////////
@@ -72,19 +80,11 @@ int main() {
               //辅助函数
 //////////////////////////////////////
 int intro() {
-  FILE *file = fopen("src/textfile/intro.txt", "r");
-
-  if (file == NULL) {
-    return 1;
-  }
-
-  char buffer[MAX_BUFFER];
 
   printf("================ 欢迎界面 ================\n");
 
-  while(fgets(buffer, MAX_BUFFER, file) != NULL) {
-    printf("%s", buffer);
-  }
+  print_file("src/textfile/intro.txt");
+
   for (int i = 0; i < 2; i++) {
     char *cmd = Command_list[i].cmd;
     char *help = Command_list[i].help_msg;
@@ -93,7 +93,6 @@ int intro() {
 
   printf("\n==========================================\n");
 
-  fclose(file);
   return 0;
 }
 
@@ -116,19 +115,43 @@ void command() {
     }
     int cmd_index = get_command_index(cmd);
     int argc = 0;
+    for (int i = 0; i < Command_list[cmd_index].max_arg; i++) {
+      args[i] = NULL;
+    }
     while ((args[argc] = strtok(NULL, " ")) != NULL && argc < Command_list[cmd_index].max_arg) {
       argc++;
     }
     if(argc > Command_list[cmd_index].max_arg) {
       continue;
     }
-    test_arg(argc, args);
+    run_command(argc, args, cmd);
   }
 }
 
 void test_arg(int argc, char *arg[]) {
   for (int i = 0; i < argc; i++) {
     printf("这个指令的第%d个参数是%s\n", i, arg[i]);
+  }
+}
+
+void run_command(int argc, char *arg[], char *cmd) {
+  if (strcmp(cmd, "q") == 0) {
+    printf("感谢游玩\n");
+    exit(1);
+  }
+  if (strcmp(cmd, "?") == 0) {
+    help(argc, arg);
+    return;
+  }
+}
+
+void help(int argc, char *arg[]) {
+  if (arg[0] == NULL) {
+    print_file("src/textfile/help_list.txt");
+    return;
+  }
+  if (strcmp(arg[0], "list") == 0) {
+    
   }
 }
 
